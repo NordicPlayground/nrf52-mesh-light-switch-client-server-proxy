4106e601c03d4845b7d53af89c48e094ec35d1c9
Torbjørn Øvrebekk <torbjorn.ovrebekk@nordicsemi.no>
2018-09-27 16:26:18 +0200

    Removed build files for experimental_dimming project

--- examples/experimental_dimming/dimming_client/build/light_switch_dimming_client_nrf52832_xxAA_s132_6.0.0_Debug/obj/packed_index_list.d
+++
@@ -1,4 +0,0 @@
-build/light_switch_dimming_client_nrf52832_xxAA_s132_6.0.0_Debug/obj/packed_index_list.o: \
- C:\NordicSemi\SDKs\nrf5_SDK_for_Mesh_v2.2.0_src\models\foundation\config\src\packed_index_list.c \
- ../../../models/foundation/config/include/packed_index_list.h \
- C:/Program\ Files\ (x86)/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.40/include/stdint.h
--- examples/experimental_dimming/dimming_server/build/light_switch_dimming_server_nrf52832_xxAA_s132_6.0.0_Debug/obj/thumb_crt0.d
+++
@@ -1,2 +0,0 @@
-build/light_switch_dimming_server_nrf52832_xxAA_s132_6.0.0_Debug/obj/thumb_crt0.o: \
- C:/Program\ Files\ (x86)/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.40/source/thumb_crt0.s
